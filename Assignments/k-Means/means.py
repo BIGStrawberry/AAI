ee2917c4b34ebc95f7a1be94a11f5b376d849815
BIGStrawberry <wouter@highserve.nl>
2019-02-22 12:27:35 +0100

    Changes to means algorithm

--- Assignments/k-Means/means.py
+++ Assignments/k-Means/means.py
@@ -1,23 +1,6 @@
-import matplotlib.pyplot as plt
 import math
 import random
-import time
-
 import numpy as np
-
-
-def get_season(date):
-    date %= 10000
-    if date < 301:
-        return "winter"
-    elif 301 <= date < 601:
-        return "lente"
-    elif 601 <= date < 901:
-        return "zomer"
-    elif 901 <= date < 1201:
-        return "herfst"
-    else:  # from 01−12 to end of year
-        return "winter"
 
 
 def get_distance(a, b, length):
@@ -32,6 +15,8 @@ def generate_random_centroids(amount, from_data):
 
     for temp_centroid in range(0, amount):
         random_data_point = random.randint(1, len(test_set))
+
+        # if random_data_point not in temp_centroids:
         temp_centroids.append(from_data[random_data_point])
 
     return temp_centroids
@@ -43,25 +28,20 @@ def prepare_cluster_list(amount):
         temp_clusters.append([])
     return temp_clusters
 
-plt.ion()
-plt.show()
 
-test_set = np.genfromtxt('dataset.csv', delimiter=';', usecols=[2,3], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
-# test_set = np.genfromtxt('dataset.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
-
-colors = np.array(10*["r", "g", "c", "b", "k"])
+# Main
+test_set = np.genfromtxt('dataset.csv', delimiter=';', usecols=[1,2,3,4,5,6,7], converters={5: lambda s: 0 if s == b"-1" else float(s), 7: lambda s: 0 if s == b"-1" else float(s)})
 
 amount_of_centroids = 4  # Amount of random centroids the program will use
 centroids = []
-if (len(centroids) < 1):
+
+if len(centroids) < 1:
     centroids = generate_random_centroids(amount_of_centroids, test_set)  # Generate N random centroids
     print("generating starting centroids")
 
-for loop in range(1, 10):
-    plt.clf()
+for loop in range(1, 25):
     print("Loop: #", loop)
 
-    plt.ylabel('Loop #' + str(loop))
     print("Using centroids: ", centroids)
     clusters = prepare_cluster_list(amount_of_centroids)  # Prepare cluster list with N centroids
 
@@ -71,26 +51,34 @@ for loop in range(1, 10):
 
         for centroid in centroids:
             distance_to_centroids.append(get_distance(data_point, centroid, len(centroid)))
-            # print("data: " , data_point)
-            # print("centroid: " , centroid)
-            # print("centroidSSS: ", centroids)
-            plt.scatter(centroid[0], centroid[1], c='k', s=10**2, marker='x')  # plot centroids (x)
 
         clusters[np.argmin(distance_to_centroids)].append(data_point)
 
     # Calculate the center of a cluster to place our centroids
+
+    # centroids = []
     for cluster in clusters:
+        # centroids.append(np.mean(cluster))
         average_point = 0
 
         for point in cluster:
-            plt.scatter(point[0], point[1], c=colors[clusters.index(cluster)], alpha=0.2)
             average_point += point
 
+        print("Avg point: ", average_point/len(cluster))
+
+        # print("Clusters: ", clusters)
+        # print("Cluster: ", cluster)
+        # print("Index: ", clusters.index(cluster))
+
+        # if cluster in clusters:
+        #     print("hallo")
+
         centroids[clusters.index(cluster)] = (average_point / len(cluster))
+        # centroids.append(average_point/len(cluster))
 
-    plt.draw()
-    plt.pause(0.001)
-
-time.sleep(100000)
-
+print("Finished.")
+print("Centroids Used: ")
+print(centroids)
+print("Clusters Found: ")
+print(clusters)
 
